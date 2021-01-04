102ca2871e24504c333b15923f28523561490aac
KurkG4 <shaggydogg6969@gmail.com>
2021-01-04 11:29:38 -0500

    change build.bat

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
-"tools/asm68k.exe" /m /p main.asm, out.md, , out.lst>out.log
-type out.log
-if not exist out.md pause & exit
-rem "tools/fixheadr.exe" out.md
-del out.log
+"tools/asm68k.exe" /m /p main.asm, s1built.bin, , s1built.lst>s1built.log
+type s1built.log
+if not exist s1built.bin pause & exit
+rem "tools/fixheadr.exe" s1built.bin
+del s1built.log
 pause
